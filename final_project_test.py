9d4f264b66e31113b12b9723a9828b128eafb69e
DeMarcus Johnson <demarcusasimpson@gmail.com>
2026-03-31 16:32:23 -0400

    Did the testing hopefully commited correctly added some simple movement and docstrings

--- final_project_test.py
+++ final_project_test.py
@@ -9,3 +9,15 @@ class Person:
         name (str): the Person's name
     """
     """Elizabeth made this commit"""
+    
+    class movement:
+        """ This is just a make believe part of the test file, but it involves movement
+        
+        Attributes:
+        x, y: character positions
+        Stamina(int): Energy for moving
+        Movement will be up, down, left, and right.
+        
+        "DeMarcus made this commit"
+        
+        """
