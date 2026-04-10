bd37c671c749b39bf1c5c8f2e1a31e0d400b5e03
Leota Wiswakarma <aileen@terpmail.umd.edu>
2026-04-10 17:21:26 -0400

    added a generate_world() funciton, returns a dictionary of a random list of coordinated tools/obstacles. imported random module

--- final_project_test.py
+++ final_project_test.py
@@ -1,6 +1,7 @@
 
 """" This is a test file for the collabrative project 
 due wednesday morning. """
+import random
 
 class Person: 
     """A person making the commit
@@ -20,3 +21,26 @@ class movement:
          """
     """ DeMarcus made this commit"""
     
+    
+    
+def generate_world():
+    rows = 10 # still unsure what to do yet for this 
+    cols = 10
+    tools = ["key", "wood_plank", "rock", "ladder", "parachute"]
+    obstacles = ["door", "lava", "dragon", "up_clif", "down_clif"]
+    
+    obstacles_info = []
+    tools_info = []
+    for o in obstacles: 
+        # adding random obstacles to info list
+        e_number = random.randint(0, 4)
+        e_item = obstacles[e_number]
+        obstacles_info.append(e_item)
+        # adding corresponding tools to info list
+        t_item = tools[e_number]
+        tools_info.append(t_item)
+
+    #dictionary for each obstacle and tool for the world
+    elements_info = {t:o for t,o in zip(tools_info, obstacles_info)}
+
+    return elements_info
